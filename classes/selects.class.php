54c235f90845cca139f372d1e8ac92c29985638f
Elenice <elenicepaterno@gmail.com>
2019-08-30 11:53:00 -0300

    Back dos filtros

--- classes/selects.class.php
+++ classes/selects.class.php
@@ -5,18 +5,57 @@ require_once "site.class.php";
 class Selects extends Site {
 
 	private $sql;
-	private $email;
 	public $resultado;
+	private $Fuma;
+	private $Aceita_fumar;
+	private $Bebe;
+	private $Aceita_beber;
+	private $Tem_animal;
+	private $Aceita_animais;
+	private $Trabalha;
+	private $Estuda;
+	private $Aceita_genero;
+	private $Aceita_pagar;
 
 
-	public function select_dados() {
+	public function __construct() {
 
-		$this->email = "vergiliopoleza@gmail.com";
-		$this->sql = "SELECT * FROM dados_usuario where Email = '$this->email'";
+		parent::__construct();
+
+		if (isset($_POST['Enviar'])) {
+
+			$this->receber_filtro();
+			
+		}
+
+	}
+
+	private function receber_filtro() {
+
+		$this->Fuma = $_POST['Fuma'];
+		$this->Bebe = $_POST['Bebe'];
+		$this->Tem_animal = $_POST['Tem_animal'];
+		$this->Trabalha = $_POST['Trabalha'];
+		$this->Estuda = $_POST['Estuda'];
+		$this->Aceita_genero = $_POST['Aceita_genero'];
+		$this->Aceita_pagar = $_POST['Aceita_pagar'];
+
+	}
+
+
+	public function select_pessoas() {
+
+		if ($this->Aceita_genero = "NI") {$this->Aceita_genero = "";} else {$this->Aceita_genero = "and $this->Aceita_genero";}
+
+		$this->sql = "SELECT * FROM dados_usuario where Fuma = $this->Fuma and Bebe = $this->Bebe and Tem_animal = $this->Tem_animal and Trabalha = $this->Trabalha $this->Aceita_genero and Estuda = $this->Estuda and Aceita_pagar <= $this->Aceita_pagar;";
 
 		if (mysqli_query($this->con, $this->sql)) {
 
 			$this->resultado = mysqli_fetch_array(mysqli_query($this->con, $this->sql));
+
+		} else {
+
+			echo "Não foi";
 
 		}
 
@@ -26,8 +65,6 @@ class Selects extends Site {
 
 
 }
-
-
 
 
 ?>
--- teste.php
+++ teste.php
@@ -3,7 +3,7 @@
 require_once "classes/selects.class.php";
 
 $teste = new Selects();
-$teste->select_dados();
+$teste->select_pessoas();
 
 
 ?>
@@ -30,7 +30,7 @@ $teste->select_dados();
 		.div_foto {
 			width: 320px;
 			height: 300px;
-			text-align: center;
+			padding: 20px 40px;
 		}
 
 		.foto_usuario {
@@ -43,8 +43,64 @@ $teste->select_dados();
 			padding: 15px;
 		}
 
+		h2 {
+			font-family: 
+		}
+
 
 	</style>
+
+	<form method="POST">
+
+		Filtros<br><br><br>
+
+		Pode fumar?<br>
+		<input type="radio" name="Fuma" id="fsim" value="1">
+		<label for="fsim">Sim</label>
+		<input type="radio" name="Fuma" id="fnao" value="0">
+		<label for="fnao">Não</label><br><br><br>
+
+		Pode beber?<br>
+		<input type="radio" name="Bebe" id="bsim" value="1">
+		<label for="bsim">Sim</label>
+		<input type="radio" name="Bebe" id="bnao" value="0">
+		<label for="bnao">Não</label><br><br><br>
+
+		Pode ter animais?<br>
+		<input type="radio" name="Tem_animal" id="tasim" value="1">
+		<label for="tasim">Sim</label>
+		<input type="radio" name="Tem_animal" id="tanao" value="0">
+		<label for="tanao">Não</label><br><br><br>
+
+		Deve trabalhar?<br>
+		<input type="radio" name="Trabalha" id="trsim" value="1">
+		<label for="trsim">Sim</label>
+		<input type="radio" name="Trabalha" id="trnao" value="0">
+		<label for="trnao">Não</label><br><br><br>
+
+		Deve estudar?<br>
+		<input type="radio" name="Estuda" id="esim" value="1">
+		<label for="esim">Sim</label>
+		<input type="radio" name="Estuda" id="enao" value="0">
+		<label for="enao">Não</label><br><br><br>
+
+		<select name="Sexo">
+			<option value="">Sexo</option>
+			<option value="Masculino">Masculino</option>
+			<option value="Feminino">Feminino</option>
+			<option value="NI">Não me importo</option>
+		</select><br><br><br>
+
+		Qual o máximo que deseja pagar?<br><br>
+		<span></span>
+		<input id="Aceita_pagar" type="range" name="Aceita_pagar" 
+		oninput="getElementById('Porcentagem').innerHTML = this.value;" 
+		min="0" max="10000" value="0" step="50" />
+		<span id="Porcentagem">0</span><br><br>
+
+		<button type="submit" name="Enviar">Enviar</button>
+
+	</form>
 
 
 	<div class="cartao">
@@ -56,6 +112,16 @@ $teste->select_dados();
 	</div>
 
 
+	<script type="text/javascript">
+
+		var $range = document.querySelector('#Aceita_pagar'),
+		$value = document.querySelector('span');
+
+		$range.addEventListener('#Aceita_pagar', function() {
+			$value.textContent = this.value;
+		});
+
+	</script>
 
 
 </body>
