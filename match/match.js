17dfd9ec1389f1e3ecf5a7caf960427e7860633a
Lucas <vintersr@gmail.com>
2019-09-03 08:33:40 -0300

    match

---
+++ match/match.js
@@ -0,0 +1,32 @@
+
+
+
+function like() {
+
+    $.ajax({
+        url: "index.php",
+        type: "POST",
+        data:onclick = like ,
+
+
+
+
+
+    }).done(function(resposta) {
+        console.log(resposta);
+
+    });
+
+
+}
+
+function deslike() {
+    $.ajax({
+        url: "index.php",
+        type: "POST",
+        data:onclick = deslike ,
+
+
+//
+//
+//     }
