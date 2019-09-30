ddbf7c3e2dd985e7b2ba0bf798dcf689dea8aba8
Gustavo <vergiliopoleza@gmail.com>
2019-09-30 20:30:49 -0300

    OH

--- gerador.php
+++ gerador.php
@@ -1,9 +1,14 @@
 <?php
 
-CONST HOST = "llamaaqui.ml:3306";
-CONST USER = "llamaaqui";
-CONST PASS = "entra21@Blusoft";
+CONST HOST = "127.0.0.1";
+CONST USER = "root";
+CONST PASS = "";
 CONST DB   = "llamaaqu_master";
+
+// CONST HOST = "llamaaqui.ml:3306";
+// CONST USER = "llamaaqui";
+// CONST PASS = "entra21@Blusoft";
+// CONST DB   = "llamaaqu_master";
 
 $con = mysqli_connect(HOST, USER, PASS, DB);
 
@@ -12,38 +17,42 @@ $sql = "SELECT * FROM gerador";
 $query = mysqli_query($con, $sql);
 $resultado = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
-// for ($i = 1; $i <= 100; $i++) {
+for ($i = 1; $i <= 1000; $i++) {
 
-$teste = array_rand($resultado);
-$resultado_nome = $resultado[$teste]['Nome'];
-$resultado_email = $resultado[$teste]['Email'];
-$senha = mt_rand(10000000, 99999999);
-$sexo = mt_rand(1, 2);
+	$randow = array_rand($resultado);
+	$resultado_nome = $resultado[$randow]['Nome'];
+	$resultado_email = $resultado[$randow]['Email']."_".mt_rand(1, 9999);
+	$senha = mt_rand(10000000, 99999999);
+	$sexo = mt_rand(1, 2);
 
-if ($sexo = 1) {
-	$sexo = 'Masculino';
-} elseif ($sexo = 2) {
-	$sexo = 'Feminino';
+	if ($sexo == 1) {
+		$sexo = 'Masculino';
+	} else {
+		$sexo = 'Feminino';
+	}
+
+
+	$dia = mt_rand(1, 29);
+	$mes = mt_rand(1, 12);
+	$ano = mt_rand(1930, 2000);
+
+	$estado = 24;
+	$cidade = mt_rand (4413, 4705);
+
+	$data = $ano."-".$mes."-".$dia;
+
+	$sql_verifica = "SELECT * FROM dados_usuario where Email = '$resultado_email'";
+	$query = mysqli_query($con, $sql_verifica);
+
+	if (mysqli_num_rows($query) == 0) {
+
+		$sql = "INSERT INTO dados_usuario (Nome, Email, Senha, Sexo, Data_nascimento, Fk_cidade, Fk_estado) values ('$resultado_nome', '$resultado_email', '$senha', '$sexo', 'Data_nascimento', '$cidade', '$estado')";
+
+		mysqli_query($con, $sql);
+
+	}
+
 }
-
-$dia = mt_rand(1, 29);
-$mes = mt_rand(1, 12);
-$ano = mt_rand(1930, 2000);
-
-$estado = 24;
-$cidade = mt_rand (4413, 4705);
-
-$data - $ano."-".$mes."-".$dia;
-
-
-$sql = "INSERT INTO dados_usuario (Nome, Email, Senha, Sexo, Data_nascimento, Fk_cidade, Fk_estado) values ('$resultado_nome', '$resultado_email', '$senha', '$sexo', 'Data_nascimento', '$cidade', '$estado')";
-
-mysqli_query($con, $sql);
-
-// $final = array ('Nome' => $resultado_nome, 'Email' => $resultado_email, 'Senha' =>$senha, 'Sexo' => $sexo, 'Dia' => $dia, 'Mes' => $mes, 'Ano' => $ano, 'Estado' => $estado, 'Cidade' => $cidade);
-
-
-// }
 
 
 ?>
