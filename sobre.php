e3aa0dc82db6ef4f5827c0ccbec27369d47cf8bc
Gustavo <vergiliopoleza@gmail.com>
2019-09-30 10:16:49 -0300

    oh

--- sobre.php
+++ sobre.php
@@ -132,9 +132,9 @@ $Iniciar = new Site;
               </div>
               <div class="card-body-d">
                 <p class="content-d color-text-a">
-                  Mineirin uai, 26 anos, músico e apaixonado por tecnologia.
-                  De uns tempos pra cá resolvi tentar a carreira na área de T.I, mais precisamente em front-end.
-                  <br> Atenção empresas! Não sou uma function( ) mas me chama que eu vou! :D
+                  Mineirin uai, 26 anos, músico, apaixonado por tecnologia desde sempre.
+                  <br>De uns tempos pra cá, resolvi tentar a carreira na área de T.I mas precisamente voltado para a parte de front end.
+                  <br> Atenção empresas... não sou uma function( ) mas me chama que eu vou! :D
                 </p>
               </div>
               <div class="card-footer-d">
