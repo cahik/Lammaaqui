94d491ce24b4d860380e567d38a075339eb46958
Gustavo <vergiliopoleza@gmail.com>
2019-10-04 09:04:29 -0300

    OH

--- chat.php
+++ chat.php
@@ -34,7 +34,7 @@ if (isset($_GET['id'])) {
     <?php require_once "include/navbar.php"; ?>
 
     <!-- Titulo -->
-    <div class="container">
+    <div class="container" style="<?php if (!isset($_GET['id'])) {echo 'margin-bottom: 90px';}?>">
         <section class="intro-single margemEsquerda">
             <div class="row">
                 <div class="col-12 mb-1 px-0">
